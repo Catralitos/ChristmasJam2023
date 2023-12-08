b5c7b41515f16fe8ce9e2d10fc397e10ccaba3e8
Zytr0nn <tomas.eufrasio.oliveira@gmail.com>
2023-12-08 22:49:59 +0000

    added better car movement

---
+++ Assets/Scripts/CarScene/RaceSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceSystem : MonoBehaviour
+{
+    public bool waypointHit = false;
+    public bool not_finished = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "waypoint")
+        {
+            waypointHit = true;
+        }
+        if (other.gameObject.tag == "finish" && waypointHit)
+        {
+            not_finished = false;
+        }
+
+    }
+}
