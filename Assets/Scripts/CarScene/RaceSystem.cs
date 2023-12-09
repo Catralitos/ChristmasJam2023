412f672201604ef8a598a6704c88cc758236835d
Zytr0nn <tomas.eufrasio.oliveira@gmail.com>
2023-12-09 19:03:36 +0000

    added leaderboard and game finish screen

--- Assets/Scripts/CarScene/RaceSystem.cs
+++ Assets/Scripts/CarScene/RaceSystem.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RaceSystem : MonoBehaviour
 {
     public bool waypointHit = false;
     public bool not_finished = true;
-
+    public GameObject FinishUI;
+    public CarSphereController carController;
+    public Timer timer;
+    public TextMeshProUGUI finalTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,9 +31,13 @@ public class RaceSystem : MonoBehaviour
             waypointHit = true;
 
         }
-        if (other.gameObject.tag == "finish" && waypointHit)
+        if (other.gameObject.tag == "finish" & waypointHit)
         {
             not_finished = false;
+            FinishUI.SetActive(true);
+            finalTimer.text = timer.timeText.text;
+            carController.DisableControls();
+            timer.StopTimer();
         }
 
     }
